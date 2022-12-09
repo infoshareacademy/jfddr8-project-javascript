a75844ee6c27d6a3d0b9718726d1c8ca8c31a719
Rafał Konieczny <rkonieczny87@gmail.com>
2022-12-09 23:07:35 +0100

    start, stop, reset

--- Project 1 (9.12)/stoper/stoper.js
+++ Project 1 (9.12)/stoper/stoper.js
@@ -1,5 +1,44 @@
+const startBtn = document.querySelector("#button-start");
+const stopBtn = document.querySelector("#button-stop");
+const resetBtn = document.querySelector("#button-reset");
+const secondsBtn = document.querySelector("#seconds");
+const tensBtn = document.querySelector("#tens");
 function Stoper() {
-    
-};
-
-Stoper();
+  let tens = 0;
+  let seconds = 0;
+  let startCounting;
+  let stopCounting;
+  let start = () => {
+    tens = tens + 1;
+    tensBtn.innerHTML = "0" + tens;
+    secondsBtn.innerHTML = "0" + seconds;
+    if (tens === 9) {
+      tens = -1;
+      seconds = seconds + 1;
+      startBtn.disabled = true;
+    }
+    if (seconds >= 10) {
+      secondsBtn.innerHTML = seconds;
+    }
+    if (seconds == 60 && tens == 0){
+      stop();
+    };
+  };
+  startCounting = setInterval(start, 10);
+  let stop = () => {
+    clearInterval(startCounting);
+    startBtn.disabled = false;
+  };
+  let reset = () => {
+    stop();
+    tensBtn.innerHTML = "00";
+    secondsBtn.innerHTML = "00";
+    tens = 0;
+    seconds = 0;
+    startBtn.disabled = false;
+  };
+  stopBtn.addEventListener("click", stop);
+  resetBtn.addEventListener("click", reset);
+  // startBtn.addEventListener("click", start);
+}
+startBtn.addEventListener("click", Stoper);
