ec9f751a0c19e6f696594570d0705518c0bdc07e
Marek Rogiński <marekroginski1994@gmail.com>
2022-12-09 20:51:43 +0100

    project1

---
+++ Project 1 (9.12)/bmi calculator/calculator.js
@@ -0,0 +1,10 @@
+
+
+
+function BMIclick()
+{
+    let height= document.getElementById('height').value;
+    let weight= document.getElementById('weight').value;
+
+    var BMIclick = weight / (height * height) ; $('#result').
+}
--- Project 1 (9.12)/stoper/stoper.js
+++ Project 1 (9.12)/stoper/stoper.js
@@ -1,5 +1,17 @@
-function Stoper() {
-    
-};
+function Stoper() 
+{   let tens=0
+const buttonStart=document.getElementById('button-start') 
+const buttonReset=document.getElementById('button-reset')
+const buttonStop=document.getElementById('button-stop')
+let Interval;
 
-Stoper();
+buttonStart.onlick=function()
+{
+Interval=setInterval(startTimer,10)
+}
+function startTimer()
+{
+tens++
+}
+
+}
