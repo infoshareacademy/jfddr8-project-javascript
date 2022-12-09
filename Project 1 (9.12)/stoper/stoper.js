1a6de12a3472da9508df2433cdc3188d5585825e
Joanna Okorowska <asia.okorowska@gmail.com>
2022-12-09 20:03:50 +0100

    zadania

--- Project 1 (9.12)/stoper/stoper.js
+++ Project 1 (9.12)/stoper/stoper.js
@@ -1,5 +1,19 @@
 function Stoper() {
-    
-};
+  let seconds = 00;
+  let tens = 00;
+  let countingTens = document.getElementById("tens").innerHTML;
+  let countingSeconds = document.getElementById("seconds").innerHTML;
+  let buttonStart = document.getElementById("button-start");
+  let buttonStop = document.getElementById("button-stop");
+  let buttonReset = document.getElementById("button-reset");
+  let Interval;
+
+  buttonStart.onclick = function () {
+    Interval = setInterval(startTimer, 10);
+  };
+  function startTimer() {
+    tens++;
+  }
+}
 
 Stoper();
--- Project 1 (9.12)/theme switcher/switcher.js
+++ Project 1 (9.12)/theme switcher/switcher.js
@@ -1,2 +1,18 @@
+const darkButton = document.querySelector(".btnDark");
+const lightButton = document.querySelector(".btnLight");
 
+darkButton.addEventListener("click", function () {
+  document.body.style.backgroundColor = "black";
+  darkButton.style.color = "white";
+  darkButton.style.borderColor = "white";
+  lightButton.style.color = "white";
+  lightButton.style.borderColor = "white";
+});
 
+lightButton.addEventListener("click", function () {
+  document.body.style.backgroundColor = "white";
+  lightButton.style.color = "black";
+  lightButton.style.borderColor = "black";
+  darkButton.style.color = "black";
+  darkButton.style.borderColor = "black";
+});
