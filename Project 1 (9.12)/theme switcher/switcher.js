c57d8235c7a3a8444dccba223f7f1da6cb4ad2c0
Rafał Konieczny <rkonieczny87@gmail.com>
2022-12-09 10:36:16 +0100

    final switcher

--- Project 1 (9.12)/theme switcher/switcher.js
+++ Project 1 (9.12)/theme switcher/switcher.js
@@ -1,2 +1,23 @@
 
+const btnDark = document.querySelector(".btnDark");
+const btnLight = document.querySelector(".btnLight");
+const body = document.querySelector("body");
+
+
+ 
+btnDark.addEventListener("click", () => {
+    body.style.backgroundColor = "black";
+    btnDark.style.color ="white";
+    btnDark.style.borderColor ="white";
+    btnLight.style.color ="white";
+    btnLight.style.borderColor ="white";
+});
+
+btnLight.addEventListener("click", () => {
+    body.style.backgroundColor = "white";
+    btnLight.style.color ="black";
+    btnLight.style.borderColor ="black";
+    btnDark.style.color ="black";
+    btnDark.style.borderColor ="black";
+});
 
