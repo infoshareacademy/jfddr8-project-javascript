0ccf13b358d91e2b7a1516c92f2a1ef98855b767
Łukasz Śmigiel <81709506+L-Smigiel@users.noreply.github.com>
2022-12-09 12:11:11 +0100

    ex 3 begun

--- Project 1 (9.12)/stoper/stoper.js
+++ Project 1 (9.12)/stoper/stoper.js
@@ -1,5 +1,9 @@
-function Stoper() {
-    
-};
+// 1. Waszym zadaniem jest wykonanie funkcjonalnego stopera w JS. HTML i CSS są już przygotowane
+// - wystarczy tylko uzupełnić funkcję Stoper() w pliku .js.
+// 2. Przycisk `start` ma rozpocząć naliczanie czasu (do setnych części sekundy),
+// `stop` zatrzymywać liczenie czasu na bieżącym wyniku, a `reset` kasować stan licznika.
+// 3. Pomocne będą funkcje "setInterval", "clearInterval" oraz "getElementById".
 
-Stoper();
+function Stoper() {
+    setInterval
+};
