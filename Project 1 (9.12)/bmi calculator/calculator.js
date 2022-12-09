8effcf954531c396cc8f49a50b351c914597c435
Rafał Konieczny <rkonieczny87@gmail.com>
2022-12-09 23:28:27 +0100

    first calculations

---
+++ Project 1 (9.12)/bmi calculator/calculator.js
@@ -0,0 +1,16 @@
+
+
+const button = document.querySelector("button");
+
+
+
+
+button.addEventListener("click", function () {
+    let height = Number(document.getElementById("height").value);
+    let weight = Number(document.getElementById("weight").value);
+    const BMI = weight / (((height / 100) * height) / 100);
+    console.log(BMI);
+  });
+
+
+  
