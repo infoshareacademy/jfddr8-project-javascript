8a34a2283a14d7bc8e00e50530ff9624dcb8a273
Patryk Makosa <makosapatryk.mk@gmail.com>
2022-12-09 11:51:02 +0100

    project01-Makosa-Patryk-1-2-3_in_progress

--- Project 1 (9.12)/stoper/stoper.js
+++ Project 1 (9.12)/stoper/stoper.js
@@ -1,5 +1,29 @@
-function Stoper() {
-    
-};
+const startBtn = document.querySelector("#button-start");
+const stopBtn = document.querySelector("#button-stop");
+const resetBtn = document.querySelector("#button-reset");
+const secondsBtn = document.querySelector("#seconds");
+const tensBtn = document.querySelector("#tens");
 
-Stoper();
+function Stoper() {
+  let tens = 0;
+  let seconds = 0;
+  let interval;
+
+  let start = () => {
+      tens = tens + 1;
+      tensBtn.innerHTML = "0" + tens;
+      if ( tens == 9){
+        tens = -1;
+      }
+      seconds = seconds + 1 ;
+    
+    };
+    setInterval(start, 1000);
+  };
+
+
+// Stoper();
+
+startBtn.addEventListener("click", Stoper);
+// stopBtn.addEventListener("click");
+// resetBtn.addEventListener("click");
--- Project 1 (9.12)/theme switcher/switcher.js
+++ Project 1 (9.12)/theme switcher/switcher.js
@@ -1,2 +1,26 @@
+const darkMode = document.querySelector('.btnDark');
+const lightMode = document.querySelector('.btnLight');
+const body = document.querySelector('body');
+const changeToDarkMode = () => {
+    body.classList.remove('lightActive');
+    body.classList.add('darkActive');   
+    darkMode.classList.remove('btnLight');
+    darkMode.classList.add('btnDark');
+    lightMode.classList.remove('btnLight');
+    lightMode.classList.add('btnDark');
+}
+const changeToLightMode = () => {
+    body.classList.remove('darkActive');
+    body.classList.add('lightActive');   
+    darkMode.classList.remove('btnDark');
+    darkMode.classList.add('btnLight');
+    lightMode.classList.remove('btnDark');
+    lightMode.classList.add('btnLight');
+}
+
+
+darkMode.addEventListener('click', changeToDarkMode );
+lightMode.addEventListener('click', changeToLightMode );
+
 
 
