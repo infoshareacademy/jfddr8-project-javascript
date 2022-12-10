b28244f8f264c6140f67c4a1176fb9ff3039181f
Agnieszka Szczepańska <szczepanska.agnieszka01@gmail.com>
2022-12-10 22:10:31 +0100

    improve calculator bmi

--- Project 1 (9.12)/bmi calculator/calculator.js
+++ Project 1 (9.12)/bmi calculator/calculator.js
@@ -4,9 +4,9 @@ const calculateBMI = document.querySelector(".btn");
 let resultofBMI = document.querySelector("#results");
 const alerts = () => {
   if (kgValue.value == 0 || cmValue.value == 0) {
-    alert("Please enter values");
+    resultofBMI.innerHTML = "Please enter values";
   } else if (kgValue.value < 0 || cmValue.value < 0)
-    alert("Negative alers are not allowed");
+    resultofBMI.innerHTML = "Negative alers are not allowed";
 };
 const calculateBMIbutton = () => {
   let yourBMI = kgValue.value / Math.pow(cmValue.value / 100, 2);
