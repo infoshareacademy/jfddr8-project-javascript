563866b7a6616708177ececa4a95594c9440fb08
Rafał Konieczny <rkonieczny87@gmail.com>
2022-12-10 22:43:30 +0100

    calculator completed

--- Project 1 (9.12)/bmi calculator/calculator.js
+++ Project 1 (9.12)/bmi calculator/calculator.js
@@ -1,16 +1,19 @@
 
 
 const button = document.querySelector("button");
-
-
-
+let resultofBMI = document.querySelector("#results");
 
 button.addEventListener("click", function () {
     let height = Number(document.getElementById("height").value);
     let weight = Number(document.getElementById("weight").value);
+    const alerts =() => {
+      if (height == 0 || weight == 0) {
+        resultofBMI.innerHTML = "Please enter values";
+      } else if (height < 0 || weight < 0) {
+        resultofBMI.innerHTML = "Negative alerts are not allowed";
+      }
+    }
     const BMI = weight / (((height / 100) * height) / 100);
-    console.log(BMI);
+    resultofBMI.innerHTML = " Your BMI is " + BMI.toFixed(1);
+    alerts()  
   });
-
-
-  
