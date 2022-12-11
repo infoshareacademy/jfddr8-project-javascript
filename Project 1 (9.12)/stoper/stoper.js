dc4d154c0f058b6a4f482b8815d14c27f70c8732
Agnieszka Szczepańska <szczepanska.agnieszka01@gmail.com>
2022-12-11 19:56:00 +0100

    final version

--- Project 1 (9.12)/stoper/stoper.js
+++ Project 1 (9.12)/stoper/stoper.js
@@ -12,18 +12,23 @@ function Stoper() {
 
   let start = () => {
     clearInterval(startCounting);
+    startCounting = setInterval(start, 10);
+
     tens = tens + 1;
     tensBtn.innerHTML = "0" + tens;
     secondsBtn.innerHTML = "0" + seconds;
 
-    startCounting = setInterval(start, 10);
-    if (tens === 9) {
-      tens = -1;
-      seconds = seconds + 1;
-      startBtn.disabled = true;
+    if (tens >= 10) {
+      tensBtn.innerHTML = tens;
     }
     if (seconds >= 10) {
       secondsBtn.innerHTML = seconds;
+    }
+
+    if (tens === 99) {
+      tens = -1;
+      seconds = seconds + 1;
+      startBtn.disabled = true;
     }
     if (seconds == 60 && tens == 0) {
       tens = 0;
