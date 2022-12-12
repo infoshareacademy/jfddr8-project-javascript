5b298fdf036063458481903c4fe5f1bb031c9b8f
Patryk Makosa <makosapatryk.mk@gmail.com>
2022-12-12 16:02:49 +0100

    Add final version 1.0

--- Project 1 (9.12)/stoper/stoper.js
+++ Project 1 (9.12)/stoper/stoper.js
@@ -3,44 +3,38 @@ const stopBtn = document.querySelector("#button-stop");
 const resetBtn = document.querySelector("#button-reset");
 const secondsBtn = document.querySelector("#seconds");
 const tensBtn = document.querySelector("#tens");
-
 function Stoper() {
   let tens = 0;
   let seconds = 0;
   let startCounting;
   let stopCounting;
-
   let start = () => {
+    clearInterval(startCounting);
+    startCounting = setInterval(start, 10);
     tens = tens + 1;
     tensBtn.innerHTML = "0" + tens;
     secondsBtn.innerHTML = "0" + seconds;
-    if (tens === 9) {
-      tens = -1;
-      seconds = seconds + 1;
-      startBtn.disabled = true;
+    if (tens >= 10) {
+      tensBtn.innerHTML = tens;
     }
     if (seconds >= 10) {
       secondsBtn.innerHTML = seconds;
     }
-    if (seconds == 60 && tens == 0){
+    if (tens === 99) {
+      tens = -1;
+      seconds = seconds + 1;
+      startBtn.disabled = true;
+    }
+    if (seconds == 60 && tens == 0) {
+      tens = 0;
+      seconds = 0;
       stop();
-    };
- 
+    }
   };
-  startCounting = setInterval(start, 10);
-
-
- 
- 
   let stop = () => {
     clearInterval(startCounting);
     startBtn.disabled = false;
- 
   };
-
-
-
-
   let reset = () => {
     stop();
     tensBtn.innerHTML = "00";
@@ -51,6 +45,6 @@ function Stoper() {
   };
   stopBtn.addEventListener("click", stop);
   resetBtn.addEventListener("click", reset);
-  // startBtn.addEventListener("click", start);
+  startBtn.addEventListener("click", start);
 }
-startBtn.addEventListener("click", Stoper);
+window.onload = Stoper();
