08d889a7e3c04f70ecb0c6f5943ea3eaac8c4c4b
Weronika Niekludow <w.niekludow@gmail.com>
2022-12-13 12:01:04 +0100

    project-02

---
+++ Project 2 (13.12)/project-2.js
@@ -0,0 +1,47 @@
+// zad 1
+//www.freecodecamp.org/news/how-to-reverse-a-string-in-javascript-in-3-different-ways-75e4763c68cb/
+
+function reverse(string) {
+  const tablica = string.split("");
+  const odwroconatablica = tablica.reverse();
+  return odwroconatablica.join("");
+}
+
+console.log(reverse("piesek"));
+
+// zad 2
+
+const str = "Marek Jurek";
+const interchangeChars = (str = "") => {
+  const [first, second] = str.split(" ");
+  const fChar = first[0];
+  const sChar = second[0];
+  const newFirst = sChar + first.substring(1, first.length);
+  const newSecond = fChar + second.substring(1, second.length);
+  const newStr = newFirst + " " + newSecond;
+  return newStr;
+};
+console.log(interchangeChars(str));
+
+// zad 3
+
+const array1 = [1, 2, 3];
+const array2 = [4, 5, 6];
+
+function diffrencevalue(a, b) {
+  const firstvalue = a.reduce((ac, cur) => ac * cur);
+  const secondvalue = b.reduce((ac, cur) => ac * cur);
+  return Math.abs(firstvalue - secondvalue);
+}
+
+console.log(diffrencevalue(array1, array2));
+
+// zad 4
+
+function 
+
+const word = "lampart";
+
+const result = word.filter(word => vowels.length);
+
+console.log(result);
