9dec3c526e8ea4516f7fd5840d259df8b63b84bf
Maja Karwacka-Frank <majakarwacka86@gmail.com>
2022-12-13 16:31:12 +0100

    Add stoper in js

--- Project 1 (9.12)/bmi calculator/calculator.js
+++ Project 1 (9.12)/bmi calculator/calculator.js
@@ -1,34 +1,3 @@
-// let bmi = document.querySelector('#results');
-// const btn = document.querySelector('.btn')
-// btn.addEventListener('click', calculateBMI)
-
-
-// function calculateBMI (weight, height){
-//     let weight = document.querySelector('#weight').value
-//     let height = document.querySelector('#height').value
-//     console.log((weight) / (height * height) *10000);
-
-
-// };
-
-
-
-// calculateBMI(168, 55);
-
-// if (height === '' || isNaN(height))
-// bmi.innerHTML = 
-
-// const button = document.querySelector(".btn");
-// button.addEventListener('click', myFunction);
-// function myFunction(){
-//     let kg = document.querySelector('#weight').value;
-//     let cm = document.querySelector('#height').value;
-//     let bmi = kg / ((cm /100) ** 2);
-//     document.querySelector('#results').innerText = bmi.toFixed[1];
-//     console.log(bmi);
-// };
-    
-
 const button = document.querySelector(".btn");
 button.addEventListener('click', myFunction);
 function myFunction(){
--- Project 1 (9.12)/theme switcher/switcher.js
+++ Project 1 (9.12)/theme switcher/switcher.js
@@ -1,19 +1,18 @@
 const btnDark = document.querySelector('.btnDark');
 const btnLight = document.querySelector('.btnLight');
-const body = document.querySelector('body')
+const body = document.querySelector('body');
 
-function dark (){
-    body.classList.add('darkMode')
-    btnDark.classList.add('border')
-    btnLight.classList.add('border')
+function dark() {
+	body.classList.add('darkMode');
+	btnDark.classList.add('border');
+	btnLight.classList.add('border');
 }
 
-function light (){
-    body.classList.remove('darkMode')
-    btnDark.classList.remove('border')
-    btnLight.classList.remove('border')
+function light() {
+	body.classList.remove('darkMode');
+	btnDark.classList.remove('border');
+	btnLight.classList.remove('border');
 }
 
-btnDark.addEventListener('click', dark)
-btnLight.addEventListener('click', light)
-
+btnDark.addEventListener('click', dark);
+btnLight.addEventListener('click', light);
