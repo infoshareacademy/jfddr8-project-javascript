02d25807f3f11e426bcaac42fc09366166f89f0c
Rafał Konieczny <rkonieczny87@gmail.com>
2022-12-13 16:11:11 +0100

    Exercise 2

--- Project 2 (13.12)/script.js
+++ Project 2 (13.12)/script.js
@@ -1,8 +1,22 @@
 // Exercise 1
 
-let reverseString = (string) =>{
-    let newString = string.split("").reverse().join("");
-    return newString;
-}
+// let reverseString = (string) =>{
+//     let newString = string.split("").reverse().join("");
+//     return newString;
+// }
 
-reverseString("piernik");
+// reverseString("piernik");
+
+// Exercise 2
+
+"Marek".charAt(0);
+"Jurek".charAt(0);
+
+function string(person) {
+  const arrayWithWords = person.split(" ")
+  const firstLetter = arrayWithWords[0][0]
+  const secondLetter = arrayWithWords[1][0]
+ return `${secondLetter+arrayWithWords[0].slice(1)} ${firstLetter+arrayWithWords[1].slice(1)}`;
+};
+
+string("Marek Jurek");
