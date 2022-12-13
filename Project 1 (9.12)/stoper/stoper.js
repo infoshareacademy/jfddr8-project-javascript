cc1478fa7d9edda8a9728675f7c16e73a246d00e
Joanna Okorowska <asia.okorowska@gmail.com>
2022-12-13 09:13:09 +0100

    changes added to stoper

--- Project 1 (9.12)/stoper/stoper.js
+++ Project 1 (9.12)/stoper/stoper.js
@@ -1,18 +1,30 @@
 function Stoper() {
-  let seconds = 00;
-  let tens = 00;
-  let countingTens = document.getElementById("tens").innerHTML;
-  let countingSeconds = document.getElementById("seconds").innerHTML;
+  let seconds = 0;
+  let tens = 0;
+  let countingTens = document.getElementById("tens");
+  let countingSeconds = document.getElementById("seconds");
   let buttonStart = document.getElementById("button-start");
   let buttonStop = document.getElementById("button-stop");
   let buttonReset = document.getElementById("button-reset");
   let Interval;
 
   buttonStart.onclick = function () {
+    clearInterval(Interval); //lub po naciśnięciu ustawiamy go jako disabled
     Interval = setInterval(startTimer, 10);
   };
+
+  buttonStop.onclick = function () {
+    clearInterval(Interval);
+  };
+
   function startTimer() {
     tens++;
+    if (tens <= 9) {
+      countingTens.innerHTML = "0" + tens;
+    }
+    if (tens > 9) {
+      countingTens.innerHTML = tens;
+    }
   }
 }
 
