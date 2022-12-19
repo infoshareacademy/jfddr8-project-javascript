ab294cc289f9bc7e38a42f26cee6f39dcf425e13
Rafał Konieczny <rkonieczny87@gmail.com>
2022-12-19 17:29:28 +0100

    Exercise 3

--- Project 2 (13.12)/script.js
+++ Project 2 (13.12)/script.js
@@ -9,14 +9,27 @@
 
 // Exercise 2
 
-"Marek".charAt(0);
-"Jurek".charAt(0);
+// "Marek".charAt(0);
+// "Jurek".charAt(0);
 
-function string(person) {
-  const arrayWithWords = person.split(" ")
-  const firstLetter = arrayWithWords[0][0]
-  const secondLetter = arrayWithWords[1][0]
- return `${secondLetter+arrayWithWords[0].slice(1)} ${firstLetter+arrayWithWords[1].slice(1)}`;
-};
+// function string(person) {
+//   const arrayWithWords = person.split(" ")
+//   const firstLetter = arrayWithWords[0][0]
+//   const secondLetter = arrayWithWords[1][0]
+//  return `${secondLetter+arrayWithWords[0].slice(1)} ${firstLetter+arrayWithWords[1].slice(1)}`;
+// };
 
-string("Marek Jurek");
+// string("Marek Jurek");
+
+// Exercise 3
+
+// let arr1 = [1, 2, 3];
+// let arr2 = [4, 2, 8];
+
+// function volumeArrays (firstArray, secondArray) {
+//   const volume1 = firstArray.reduce((firstArray, secondArray) => firstArray*secondArray);
+//   const volume2 = secondArray.reduce((firstArray, secondArray) => firstArray*secondArray);
+//   const arraysVolume = volume1 - volume2;
+//   return Math.abs(arraysVolume);
+// }
+// console.log(volumeArrays(arr1, arr2));
