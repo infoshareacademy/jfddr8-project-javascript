e3a3abcb18074f10f60056946b5e4b610dae52de
Rafał Konieczny <rkonieczny87@gmail.com>
2022-12-27 22:28:38 +0100

    programmed table generation and background color change when clicking on a field

---
+++ Project 3 (15.12)/script.js
@@ -0,0 +1,31 @@
+function createTable() {  
+    const row = document.getElementById("row").value;
+    const col = document.getElementById("col").value;
+    const table = document.createElement("table");
+    const tblBody = document.createElement("tbody");
+  
+    for (let r=0; r < row; r++) {
+      const row = document.createElement("tr");
+  
+      for(let c=0; c < col; c++) {
+        const cell = document.createElement("td");
+        const cellText = document.createTextNode(`cell in row ${r}, column ${c}`);
+        cell.appendChild(cellText);
+        row.appendChild(cell);
+      }
+  
+      table.appendChild(row);
+    }
+  
+    table.appendChild(tblBody);
+    document.body.appendChild(table);
+    table.setAttribute("border", "2");
+
+    const targetCells = document.querySelectorAll("td");
+    targetCells.forEach((element) =>
+    element.addEventListener("click", () => {
+    element.style.background === "yellow" ? element.style.background = "transparent" : element.style.background = "yellow"
+    })
+  );
+}
+
