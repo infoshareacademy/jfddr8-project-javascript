ef42d530c8948396b00d76dc99598ced19798407
Agnieszka Szczepańska <szczepanska.agnieszka01@gmail.com>
2022-12-29 11:50:41 +0100

    Second task

--- Project 2 (13.12)/script.js
+++ Project 2 (13.12)/script.js
@@ -5,3 +5,16 @@ const reverseString = (text) => {
 };
 
 reverseString("Agnieszka");
+
+//2nd task
+const changeFirstCharacter = (string) => {
+  const firstLetterFirstWord = string.split(" ")[0].slice(0, 1);
+  const firstLetterSecondWord = string.split(" ")[1].slice(0, 1);
+
+  const newFirstWord = firstLetterSecondWord + string.split(" ")[0].slice(1);
+  const newSecondWord = firstLetterFirstWord + string.split(" ")[1].slice(1);
+
+  console.log(newFirstWord + " " + newSecondWord);
+};
+
+changeFirstCharacter("Marek Jurek");
