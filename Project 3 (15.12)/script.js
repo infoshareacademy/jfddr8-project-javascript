826d6c02c16dcaa84fe303e04e969e877c87696a
Agnieszka Szczepańska <szczepanska.agnieszka01@gmail.com>
2022-12-30 12:04:13 +0100

    add number validation

--- Project 3 (15.12)/script.js
+++ Project 3 (15.12)/script.js
@@ -1,29 +1,49 @@
 // dać w html max ilość kolumn i rzędów
 //createTextNode do punktu 3go
 
-const rows = document.getElementById("rows");
-const cols = document.getElementById("cols");
-const generate = document.getElementById("generate");
-const tableContainer = document.getElementById("table");
+const rowsInput = document.getElementById("rows");
+const colsInput = document.getElementById("cols");
+const generateBtn = document.getElementById("generate");
+let tableContainer = document.getElementById("table");
+
+let tableRow;
+let tableCol;
 
 const addTable = (event) => {
   event.preventDefault();
-  let tableRow;
+  tableContainer.innerHTML = '<table id="table"></table>';
 
-  for (let iRow = 1; iRow <= rows.value; iRow++) {
+  for (let iRow = 1; iRow <= rowsInput.value; iRow++) {
     tableRow = document.createElement("tr");
-    tableContainer.appendChild(tableRow);
+    a = tableContainer.appendChild(tableRow);
 
-    for (let iCol = 1; iCol <= cols.value; iCol++) {
+    for (let iCol = 1; iCol <= colsInput.value; iCol++) {
       let tableCol = document.createElement("td");
       tableCol.textContent = "cell in row" + iRow + ",column" + iCol;
-      tableRow.style.backgroundColor = "yellow";
+      tableCol.style.cursor = "pointer";
       tableRow.append(tableCol);
       tableCol.style.border = "2px solid black";
     }
   }
-
-  console.log(tableRow);
 };
 
-generate.addEventListener("click", addTable);
+const checkNumber = (event) => {
+  const alert = document.querySelector("aside");
+  alert.style.color = "red";
+  alert.innerText = "";
+  if (rowsInput.value < 0 || colsInput.value < 0) {
+    alert.innerText = "Incorrect number. Enter a positive value!";
+  } else if (rowsInput.value > 10 || colsInput.value > 10) {
+    alert.innerText = "Please, enter a number that is less than 10";
+    event.stopImmediatePropagation();
+  }
+};
+
+const changeColor = () => {
+  const cell = document.querySelectorAll("td");
+  cell.style.backgroundColor = "yellow";
+};
+
+generateBtn.addEventListener("click", checkNumber);
+generateBtn.addEventListener("click", addTable);
+tableContainer.addEventListener("click", changeColor);
