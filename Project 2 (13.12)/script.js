6ab2f441b62040271f3ecc33cf2cb8c76a5835a2
ryczbo <ryczbo09@gmail.com>
2023-01-05 15:40:39 +0100

    upload solution for project 2

--- Project 2 (13.12)/script.js
+++ Project 2 (13.12)/script.js
@@ -1,98 +1,84 @@
-//          1st task
-const reverseString = (text) => {
-  const newText = text.split("").reverse().join("");
-  return newText;
-};
-
-reverseString("Agnieszka");
-
-//            2nd task
-const changeFirstCharacter = (string) => {
-  const firstLetterFirstWord = string.split(" ")[0].slice(0, 1);
-  const firstLetterSecondWord = string.split(" ")[1].slice(0, 1);
-
-  const newFirstWord = firstLetterSecondWord + string.split(" ")[0].slice(1);
-  const newSecondWord = firstLetterFirstWord + string.split(" ")[1].slice(1);
-
-  const result = newFirstWord + " " + newSecondWord;
-  return result;
-};
-
-changeFirstCharacter("Marek Jurek");
-
-//          3rd task
-const volumeDifference = () => {
-  const firstArr = [1, 2, 3];
-  const secondArr = [4, 2, 8];
-
-  const firstVolume = firstArr.reduce(
-    (volume, currentValue) => volume * currentValue,
-    1
-  );
-
-  const secondVolume = secondArr.reduce(
-    (volume, currentValue) => volume * currentValue,
-    1
-  );
-
-  let result;
-  if (firstVolume >= secondVolume) {
-    result = firstVolume - secondVolume;
-  } else {
-    result = secondVolume - firstVolume;
+// 1 string reverse
+function solution(str){
+    return str.split('').reverse().join('');  
   }
 
-  console.log(result);
-  return result;
-};
+// alt
+const solution = str => [...str].reverse().join('');
 
-volumeDifference();
 
-//      4th task
-
-const vowel = (string) => {
-  const vowel = ["a", "ą", "e", "ę", "i", "o", "ó", "u", "y"];
-  const letters = string.split("").map((i) => i.toLowerCase());
-  let vowelsArr = [];
-
-  for (let i = 0; i < vowel.length; i++) {
-    vowelsArr = vowelsArr.concat(
-      letters.filter((letter) => letter.includes(vowel[i]))
-    );
+// alt2
+function solution(str){
+    var newStr = '';
+    for (var i = str.length - 1; i >= 0; i--)
+      newStr += str[i];
+    return newStr;
   }
-  console.log("Number of vowel:", vowelsArr.length);
-  return vowelsArr.length;
+
+// 2 swap letters
+function swap(words) {
+  const [firstWord, secondWord] = words.split(' ')
+  return `${secondWord[0]+firstWord.slice(1)} ${firstWord[0]+secondWord.slice(1)}`
+}
+
+//3 z podanych dwóch arrayek z trzema wymiarami, np. [1, 2, 3] i [4, 2, 8] oblicz objetosci i podaj róznicę (wiekszy - mniejszy);
+
+function findDifference(a, b) {
+  const firstSum = a.reduce((a, b) => a*b);
+  const secondSum = b.reduce((a, b) => a*b);
+  
+  return Math.abs(firstSum - secondSum);
+}
+
+function findDifference(a, b) {
+  return Math.abs(a.reduce((a, b) => a*b) - b.reduce((a, b) => a*b))
+}
+
+//4 vowels number
+function vowelsNumber(str) {
+  const vowels = ['a', 'e', 'i', 'o', 'u', 'y'];
+  let count = 0;
+  
+  const arr = str.split("");
+  arr.forEach(el => {
+    if (vowels.includes(el)) {
+      count++
+    }
+  })
+  return count;
 };
 
-vowel("Agnieszka");
+//alt
 
-//5th task
-
-const transformArray = (number, arr) => {
-  const arrFromBigger = arr.sort((a, b) => {
-    return b - a;
-  });
-  const newArray = arrFromBigger.slice(0, number);
-  return newArray;
+function vowelsNumber(str) {
+  return str.split("").filter(letter => "aeiouy".includes(letter)).length
 };
 
-transformArray(2, [5, 3, 1, 4, 2]);
+//5 largest from array with given amount of largest items
+function largest(n,xs){
+  xs.sort((a, b) => b - a).splice(n)
+  return xs
+}
 
-//6th task
 
-const shareReceipt = (list) => {
-  const amount = Object.values(list);
-  const amountPerPerson =
-    amount.reduce((sum, price) => sum + price, 0) / amount.length;
+//alt
+function largest(n,xs){
+  const result = xs.sort((a , b) => b - a)
+  result.length = n
 
-  const result = Object.fromEntries(
-    Object.entries(list).map(([key, val]) => [key, val - amountPerPerson])
-  );
-  return result;
-};
+  return result
+}
 
-shareReceipt({
-  George: 15,
-  Bettie: 10,
-  Anna: 5,
-});
+//6 bill split
+function splitTheBill(x) {
+    const evenPart = Object.values(x).reduce((a, b) => a + b, 0) / Object.values(x).length;
+  
+    Object.keys(x).forEach(key => {
+      x[key] = x[key] - evenPart;
+    })
+  
+  return x;
+
+}
+
+
