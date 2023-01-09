0d8f451d074627ddeb23eb30bdb58b1e77ed7d3a
Patryk Makosa <makosapatryk.mk@gmail.com>
2023-01-09 10:20:49 +0100

    1-2 challenge done

---
+++ Project 4 (9.01)/script.js
@@ -0,0 +1,36 @@
+const productElement = document.querySelector(".products");
+const favoriteElement = document.querySelector(".favorites");
+
+// const fetchProducts = () => {
+//   fetch("https://dummyjson.com/products")
+//     .then((response) => response.json())
+//     .then((results) => {
+//       const products = results.products;
+//       products.forEach((products) => products.appendChild(productElement))
+      
+//     });
+// };
+
+
+const fetchProducts = () => {
+    fetch('https://dummyjson.com/products')
+      .then(response => response.json())
+      .then(results => {
+        const products = results.products;
+        const productUL = document.createElement('ul')
+        productElement.appendChild(productUL)
+        products.forEach(product => {
+          const productLi = document.createElement('li');
+          productLi.innerHTML = `
+            <span>${product.title}</span>
+            <span>${product.price}</span>
+            <button class='button'>Add to favorite</button>
+            `;
+  
+     
+          productUL.appendChild(productLi);
+        });
+      });
+  };
+
+  fetchProducts()
