cf9078eb5f883ed02751cfa9f92338c63a4e81f9
Rafał Konieczny <rkonieczny87@gmail.com>
2023-01-09 12:02:57 +0100

    created addToFavorite and localStorageKey

--- Project 4 (9.01)/script.js
+++ Project 4 (9.01)/script.js
@@ -1,21 +1,22 @@
 const productElement = document.querySelector(".products");
 const favoriteElement = document.querySelector(".favorites");
+const localStorageKey = "FavoriteProducts";
 
 const fetchProducts = () => {
   fetch("https://dummyjson.com/products")
     .then((response) => response.json())
     .then((results) => {
-      const products = results.products;
-      const productUL = document.createElement("ul");
-      productUL.classList.add("cos");
-      productElement.appendChild(productUL);
-      products.forEach((product) => {
-        const productLi = document.createElement("li");
-        const button = document.createElement("button");
-        button.innerText = "Add to favorite";
-        button.addEventListener("click", addToFavorite(product));
-        productLi.innerText = product.title + " " + product.price
-        productLi.append(button)
+        const products = results.products;
+        const productUL = document.createElement("ul");
+        productUL.classList.add("cos");
+        productElement.appendChild(productUL);
+        products.forEach((product) => {
+            const productLi = document.createElement("li");
+            const button = document.createElement("button");
+            button.innerText = "Add to favorite";
+            button.addEventListener("click", () => addToFavorite(product));
+            productLi.innerText = product.title + " " + product.price;
+            productLi.append(button);
 
         productUL.appendChild(productLi);
       });
@@ -23,4 +24,8 @@ const fetchProducts = () => {
 };
 
 fetchProducts();
-const addToFavorite = (product) => {};
+const addToFavorite = (product) => {
+    const favoriteProducts = JSON.parse(localStorage.getItem(localStorageKey)) || [];
+    favoriteProducts.push(product);
+    localStorage.setItem(localStorageKey,JSON.stringify(favoriteProducts))   
+};
