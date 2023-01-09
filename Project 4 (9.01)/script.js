b6cc4f22cd83b8f1469de8e0081d9de1c3e9ceea
Rafał Konieczny <rkonieczny87@gmail.com>
2023-01-09 10:52:37 +0100

    change in code and add button

--- Project 4 (9.01)/script.js
+++ Project 4 (9.01)/script.js
@@ -2,24 +2,25 @@ const productElement = document.querySelector(".products");
 const favoriteElement = document.querySelector(".favorites");
 
 const fetchProducts = () => {
-    fetch('https://dummyjson.com/products')
-      .then(response => response.json())
-      .then(results => {
-        const products = results.products;
-        const productUL = document.createElement('ul')
-        productElement.appendChild(productUL)
-        products.forEach(product => {
-          const productLi = document.createElement('li');
-          productLi.innerHTML = `
-            <span>${product.title}</span>
-            <span>${product.price}</span>
-            <button class='button'>Add to favorite</button>
-            `;
-  
+  fetch("https://dummyjson.com/products")
+    .then((response) => response.json())
+    .then((results) => {
+      const products = results.products;
+      const productUL = document.createElement("ul");
+      productUL.classList.add("cos");
+      productElement.appendChild(productUL);
+      products.forEach((product) => {
+        const productLi = document.createElement("li");
+        const button = document.createElement("button");
+        button.innerText = "Add to favorite";
+        button.addEventListener("click", addToFavorite(product));
+        productLi.innerText = product.title + " " + product.price
+        productLi.append(button)
 
-          productUL.appendChild(productLi);
-        });
+        productUL.appendChild(productLi);
       });
-  };
+    });
+};
 
-  fetchProducts()
+fetchProducts();
+const addToFavorite = (product) => {};
