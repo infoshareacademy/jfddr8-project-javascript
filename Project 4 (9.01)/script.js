a16c3c6f83c87530593cc787782913ae9f0d1355
Patryk Makosa <makosapatryk.mk@gmail.com>
2023-01-09 11:51:04 +0100

    cos zrobione

--- Project 4 (9.01)/script.js
+++ Project 4 (9.01)/script.js
@@ -1,36 +1,34 @@
 const productElement = document.querySelector(".products");
 const favoriteElement = document.querySelector(".favorites");
-
-// const fetchProducts = () => {
-//   fetch("https://dummyjson.com/products")
-//     .then((response) => response.json())
-//     .then((results) => {
-//       const products = results.products;
-//       products.forEach((products) => products.appendChild(productElement))
-      
-//     });
-// };
-
+const localStorageKey = "FavoriteProducts";
 
 const fetchProducts = () => {
-    fetch('https://dummyjson.com/products')
-      .then(response => response.json())
-      .then(results => {
-        const products = results.products;
-        const productUL = document.createElement('ul')
-        productElement.appendChild(productUL)
-        products.forEach(product => {
-          const productLi = document.createElement('li');
-          productLi.innerHTML = `
-            <span>${product.title}</span>
-            <span>${product.price}</span>
-            <button class='button'>Add to favorite</button>
-            `;
-  
-     
-          productUL.appendChild(productLi);
-        });
-      });
-  };
+  fetch("https://dummyjson.com/products")
+    .then((response) => response.json())
+    .then((results) => {
+      const products = results.products;
+      const productUL = document.createElement("ul");
+      productUL.classList.add("cos");
+      productElement.appendChild(productUL);
+      products.forEach((product) => {
+        const productLi = document.createElement("li");
+        const button = document.createElement("button");
+        button.innerText = "Add to favorite";
+        button.addEventListener("click", () => addToFavorite(product));
+        productLi.innerText = product.title + " " + product.price;
+        productLi.append(button);
 
-  fetchProducts()
+        productUL.appendChild(productLi);
+      });
+    });
+};
+
+fetchProducts();
+const addToFavorite = (product) => {
+    
+    const favoriteProducts = JSON.parse(localStorage.getItem(localStorageKey)) || [];
+    favoriteProducts.push(product);
+    localStorage.setItem(localStorageKey,JSON.stringify(favoriteProducts))
+      favoriteProducts.includes(product) ? console.log('true') : console.log('o cie Uj')
+    
+};
