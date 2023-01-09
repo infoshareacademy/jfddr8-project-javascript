a632b74c185c8b0f24152f21442ac51cdf51af6a
Rafał Konieczny <rkonieczny87@gmail.com>
2023-01-09 10:24:47 +0100

    1 and 2 points done

---
+++ Project 4 (9.01)/script.js
@@ -0,0 +1,25 @@
+const productElement = document.querySelector(".products");
+const favoriteElement = document.querySelector(".favorites");
+
+const fetchProducts = () => {
+    fetch('https://dummyjson.com/products')
+      .then(response => response.json())
+      .then(results => {
+        const products = results.products;
+        const productUL = document.createElement('ul')
+        productElement.appendChild(productUL)
+        products.forEach(product => {
+          const productLi = document.createElement('li');
+          productLi.innerHTML = `
+            <span>${product.title}</span>
+            <span>${product.price}</span>
+            <button class='button'>Add to favorite</button>
+            `;
+  
+
+          productUL.appendChild(productLi);
+        });
+      });
+  };
+
+  fetchProducts()
