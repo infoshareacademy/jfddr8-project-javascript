d746b9d2dc242fd2662d039ef097a4f65a5688e0
Maja Karwacka-Frank <majakarwacka86@gmail.com>
2023-01-09 20:29:24 +0100

    Add solution to project4

--- Project 4 (9.01)/script.js
+++ Project 4 (9.01)/script.js
@@ -1,12 +1,10 @@
 const btns = document.getElementsByClassName('.btn');
 const ulProducts = document.querySelector('.ul-products');
 const ulFavorites = document.querySelector('.ul-favorites');
-const ulLocal = document.querySelector('.ul-local');
 
 const URL = 'https://dummyjson.com/products';
 
 let productID = 0;
-const object = {};
 
 const getApi = () => {
 	fetch(URL)
@@ -19,19 +17,10 @@ const getApi = () => {
 				const price = products[i].price;
 				const divLI = document.createElement('div');
 				divLI.setAttribute('class', 'box-products');
-
-				divLI.innerHTML = `<li id="${productID}">Title:${title}, Price: ${price}</li>
+				divLI.innerHTML = `<li id="${productID}"> Title: ${title}, Price: ${price}</li>
                 <button class="btn" onclick="addToLocal(${productID})">Dodaj do ulubionych</button>`;
-
 				ulProducts.appendChild(divLI);
 				productID++;
-
-
-
-                // localStorage.setItem(`${title}`, `${price}`)
-
-
-                
 			}
 		});
 };
@@ -41,24 +30,17 @@ getApi();
 const addToLocal = (id) => {
 	const liToLocal = document.getElementById(id);
 	const text = liToLocal.textContent;
-    
-
-	if (!ulLocal.textContent.includes(text)) {
-		const newLiToLocal = document.createElement('li');
-		newLiToLocal.textContent = text;
-        localStorage.setItem(text, JSON.stringify(text));
-		ulLocal.appendChild(newLiToLocal);
-
-	} else {
+	const arr = Object.keys(localStorage);
+	
+	if (arr.includes(text)) {
 		alert('Ten produkt jest już w ulubionych!');
+	} else {
+		localStorage.setItem(text, text);
 	}
 };
 
 const addToFav = () => {
-	ulFavorites.innerHTML = ulLocal.innerHTML;
-	// ulFavorites.innerHTML = localStorage
-	
-
+	ulFavorites.innerHTML = Object.keys(localStorage);
 };
 
 setInterval(addToFav, 5000);
