0b2ad650b19f597500703a398ba3c26c2dab4c4b
Sebastian Majda <sebastianmajda90@gmail.com>
2023-01-10 20:48:00 +0100

    Projekt-4/Sebastian-Majda Final Commit

--- Project 4 (9.01)/app.js
+++ Project 4 (9.01)/app.js
@@ -18,3 +18,44 @@ async function loadProducts() {
     list.appendChild(listItem);
   });
 }
+
+function addToFavorites(product) {
+  return () => {
+    const data = localStorage.getItem("favorites");
+    if (!data) {
+      localStorage.setItem("favorites", JSON.stringify([product]));
+      return;
+    }
+    const favorites = JSON.parse(data);
+    if (favorites.find((favorite) => favorite.id === product.id)) {
+      alert("Product already added.");
+      return;
+    }
+    favorites.push(product);
+    localStorage.setItem("favorites", JSON.stringify(favorites));
+  };
+}
+
+function loadFavorites() {
+  const data = localStorage.getItem("favorites");
+  if (!data) {
+    return;
+  }
+  const favorites = JSON.parse(data);
+  const favoritesList = document.querySelectorAll("ul")[1];
+  while (favoritesList.lastChild) {
+    favoritesList.removeChild(favoritesList.lastChild);
+  }
+  favorites.forEach((product) => {
+    const listItem = document.createElement("li");
+    const title = document.createElement("h3");
+    title.innerText = product.title;
+    const price = document.createElement("div");
+    price.innerText = `$${product.price}`;
+    listItem.appendChild(title);
+    listItem.appendChild(price);
+    favoritesList.appendChild(listItem);
+  });
+}
+loadProducts();
+setInterval(loadFavorites, 5000);
