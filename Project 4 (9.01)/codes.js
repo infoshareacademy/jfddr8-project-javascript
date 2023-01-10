f278053ce66e5e9026437d95cc440f8b0da9ca4f
olga-kacala <olga.kacala@gmail.com>
2023-01-10 09:02:03 +0100

    Project 4 added

--- Project 4 (9.01)/codes.js
+++ Project 4 (9.01)/codes.js
@@ -1,28 +1,91 @@
-const data = document.getElementById('myList');
+// const data = document.getElementById('myList');
 
-const localStorageKey = 'favouriteProducts';
+// const localStorageKey = 'favouriteProducts';
 
-fetch('https://dummyjson.com/products')
-.then((res)=> res.json())
-.then((res)=> {
-res.products.forEach(function(element) {
-    var titles = document.createElement('li');
-    var price = document.createElement('p');
-    var btn = document.createElement('button');
+// fetch('https://dummyjson.com/products')
+// .then((res)=> res.json())
+// .then((res)=> {
+// res.products.forEach(function(element) {
+//     var titles = document.createElement('li');
+//     var price = document.createElement('p');
+//     var btn = document.createElement('button');
  
-   titles.innerHTML = element.title +', ' +element.price;
-   btn.innerText = 'Dodaj do ulubionych';
+//    titles.innerHTML = element.title +', ' +element.price;
+//    btn.innerText = 'Dodaj do ulubionych';
     
-   data.appendChild(titles);
-   data.appendChild(btn);
+//    data.appendChild(titles);
+//    data.appendChild(btn);
 
-   btn.addEventListener('click', () => {
-    const favouriteProducts = JSON.parse(localStorage.getItem(localStorageKey)) || [];
-    favouriteProducts.push(element);
-    localStorage.setItem(localStorageKey, JSON.stringify(favouriteProducts));
-})
-})});
+//    btn.addEventListener('click', () => {
+//     const favouriteProducts = JSON.parse(localStorage.getItem(localStorageKey)) || [];
+//     favouriteProducts.push(element);
+//     localStorage.setItem(localStorageKey, JSON.stringify(favouriteProducts));
+// })
+// })});
 
+
+let updateFavorites = () => {
+    const localStorageKey = "favoriteProducts";
+    let favorites = document.getElementById("myFavorites");
+    favorites.innerHTML = '';
+    let favoriteProducts =
+          JSON.parse(localStorage.getItem(localStorageKey)) || [];
+          for (const favoriteProduct of favoriteProducts) {
+            let fav = document.createElement('li');
+            fav.innerHTML = favoriteProduct.title + " - " + favoriteProduct.price;
+            favorites.appendChild(fav);
+          }
+}
+
+fetch("https://dummyjson.com/products")
+  .then((res) => res.json())
+  .then((res) => {
+    const data = document.getElementById("myList");
+
+    res.products.forEach(function (product) {
+      var title = document.createElement("li");
+      var btn = document.createElement("button");
+
+      const localStorageKey = "favoriteProducts";
+
+      title.innerHTML = product.title + " - " + product.price;
+      btn.innerText = "Dodaj do ulubionych";
+
+      data.appendChild(title);
+      data.appendChild(btn);
+
+      btn.addEventListener("click", () => {
+
+        let favoriteProducts =
+          JSON.parse(localStorage.getItem(localStorageKey)) || [];
+        
+          let isDuplicate = false;
+
+            for (const favoriteProduct of favoriteProducts) {
+                console.log(favoriteProduct)
+                if (favoriteProduct.id === product.id) {
+                    alert("Produkt już dodany do ulubionych");
+                    isDuplicate = true;
+                    break;
+                }
+              }
+
+            if (!isDuplicate) {
+                    favoriteProducts.push(product);
+                    localStorage.setItem(
+                        localStorageKey,
+                        JSON.stringify(favoriteProducts)
+                    );
+                    updateFavorites()
+            }
+
+      });
+    });
+  });
+
+  setInterval(function () {
+    updateFavorites()
+  }, 5000);
 
 
 
