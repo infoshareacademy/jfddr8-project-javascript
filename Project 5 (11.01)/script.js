c1e151a4ecee06aefc08829c6a09261da1a12d46
Rafał Konieczny <rkonieczny87@gmail.com>
2023-01-11 21:22:46 +0100

    3 i 4 point done

--- Project 5 (11.01)/script.js
+++ Project 5 (11.01)/script.js
@@ -2,6 +2,7 @@ const containerData = document.querySelector('#data');
 const addButton = document.querySelector('#addMe');
 const inputUser = document.querySelector('#username');
 
+const fetchUsers = () => {
 fetch('https://reqres.in/api/users')
     .then(response => response.json())
     .then(result => {
@@ -18,16 +19,22 @@ fetch('https://reqres.in/api/users')
     .catch(error => {
         console.log(error)
     });
-    
-const listOfUsers = () => (containerData.innerHTML = '');
-listOfUsers();
+};
 
 addUsers = () => {
-    fetch('https://reqres.in/api/users'),{
-        method: 'POST',
-        headers: { 'Content-Type': "application/json"},
-        body: JSON.stringify({username}),
-    }
-}
-
-addButton.addEventListener('click', addUsers)
+    const name = inputUser.value;
+    console.log(name);
+    const innerUserElement = document.createElement("div");
+    innerUserElement.innerHTML = `
+          <img width='128px' height='128px'src='https://images.pexels.com/photos/1704488/pexels-photo-1704488.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'></img> ${name} 
+      `;
+      containerData.append(innerUserElement)
+    fetch("https://reqres.in/api/users", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ name }),
+    });
+};
+  
+addButton.addEventListener("click", addUsers);
+fetchUsers();
