e83c94aab78fbb991d196d10fdded1584d509116
Patryk Makosa <makosapatryk.mk@gmail.com>
2023-01-11 11:41:52 +0100

    1-2 challenge done ( for party )

--- Project 5 (11.01)/script.js
+++ Project 5 (11.01)/script.js
@@ -1,34 +1,38 @@
 const containerData = document.querySelector("#data");
+const addButton = document.querySelector("#addMe");
+const inputUser = document.querySelector("#username");
 
-fetch("https://reqres.in/api/users")
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (result) {
-    console.log(result.data);
-    const usersData = result.data;
-    const userElement = usersData.map((element) => {
-      const innerUserElement = document.createElement("div");
-      innerUserElement.innerHTML = `
+const fetchUsers = () => {
+  fetch("https://reqres.in/api/users")
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (result) {
+      console.log(result.data);
+      const usersData = result.data;
+      const userElement = usersData.map((element) => {
+        const innerUserElement = document.createElement("div");
+        innerUserElement.innerHTML = `
         <img src=${element.avatar}></img> ${element.first_name}  ${element.last_name}  
     `;
-      return innerUserElement;
+        return innerUserElement;
+      });
+      containerData.append(...userElement);
+    })
+    .catch(function (error) {
+      console.log(error);
     });
-    containerData.append(...userElement);
-  })
-  .catch(function (error) {
-    console.log(error);
+};
+
+addUsers = (username) => {
+  fetch("https://reqres.in/api/users", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ username }),
+  }).then(() => {
+    fetchUsers()
   });
+};
 
-
-
-
-
-
-
-
-
-
-
-
-
+addButton.addEventListener("click", addUsers);
+fetchUsers()
