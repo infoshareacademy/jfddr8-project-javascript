08880b3e56eb861c1e52bc79ab96c10665c7461d
Agnieszka Szczepańska <szczepanska.agnieszka01@gmail.com>
2023-01-11 21:03:20 +0100

    complited task 4

--- Project 5 (11.01)/script.js
+++ Project 5 (11.01)/script.js
@@ -23,13 +23,20 @@ const fetchUser = () => {
 };
 fetchUser();
 
-addUsers = (username) => {
-  fetch("https://reqres.in/api/users"),
-    {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ username }),
-    };
+addUsers = () => {
+  const name = inputUser.value;
+  console.log(name);
+  const innerUserElement = document.createElement("div");
+  innerUserElement.innerHTML = `
+        <img width='128px' height='128px'src='https://images.pexels.com/photos/1704488/pexels-photo-1704488.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'></img> ${name} 
+    `;
+  containerData.append(innerUserElement);
+  fetch("https://reqres.in/api/users", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ name }),
+  });
 };
 
 addButton.addEventListener("click", addUsers);
+fetchUser();
